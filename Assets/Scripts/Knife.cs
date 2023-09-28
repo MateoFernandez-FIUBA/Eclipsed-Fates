2e4ae2aa7ee2e2df5d98972f8f37d1c1274a12d7
SimM20 <simon.moraleslozano@live.com>
2023-09-28 19:37:09 -0300

    28-9

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -7,18 +7,27 @@ public class Knife : MonoBehaviour
     [SerializeField] private Transform Weapon;
     [SerializeField] private float radius;
     [SerializeField] private float damage;
+    [SerializeField] private float timeBtwAttack;
+    [SerializeField] private float timeForFollowAttack;
 
     private void Start()
     {
         radius = 0.5f;
-        damage = 100f;
+        damage = 10f;
+        timeBtwAttack = 1f;
     }
 
     private void Update()
     {
+        if (timeForFollowAttack > 0)
+        {
+            timeForFollowAttack -= Time.deltaTime;
+        }
+
         if (Input.GetButtonDown("Fire2"))
         {
             Attack();
+            timeForFollowAttack = timeBtwAttack;
         }
     }
 
