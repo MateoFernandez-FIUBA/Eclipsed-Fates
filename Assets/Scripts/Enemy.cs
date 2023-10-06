af7d8bfcda1c08d52a9accd442941dc714184122
SimM20 <simon.moraleslozano@live.com>
2023-10-06 02:31:34 -0300

    06-10

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,13 +11,10 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float speed;
     [SerializeField] private float damage;
-
-    private void Start()
-    {
-        life = 10;
-        speed = 1.5f;
-        damage = 25;
-    }
+    [SerializeField] private float detectionRange;
+    private bool hasSeenPlayer = false;
+    private float timeSinceLastSight = 0f;
+    [SerializeField] private float timeToRememberPlayer;
 
     private void Update()
     {
@@ -25,23 +22,45 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        float angRad = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x);
+        if (target != null)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+            if (distanceToPlayer <= detectionRange) 
+            {
+                float angRad = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x);
 
-        float angGrad = (180 / Mathf.PI) * angRad - 90;
+                float angGrad = (180 / Mathf.PI) * angRad - 90;
 
-        transform.rotation = Quaternion.Euler(0, 0, angGrad);
+                transform.rotation = Quaternion.Euler(0, 0, angGrad);
 
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+
+                hasSeenPlayer = true;
+                timeSinceLastSight = 0f;
+            }
+        }
+        else
+        {
+            // Si el enemigo ha visto al jugador, cuenta el tiempo desde la última vista
+            if (hasSeenPlayer)
+            {
+                timeSinceLastSight += Time.deltaTime;
+
+                // Si ha pasado suficiente tiempo, deja de recordar al jugador
+                if (timeSinceLastSight >= timeToRememberPlayer)
+                {
+                    hasSeenPlayer = false;
+                }
+            }
+        }
     }
-
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Character>().TakeDamage(damage*Time.deltaTime);
+            collision.gameObject.GetComponent<Character>().TakeDamage(damage * Time.deltaTime);
         }
     }
-
     public void TakeDamage(float damage)
     {
         life -= damage;
